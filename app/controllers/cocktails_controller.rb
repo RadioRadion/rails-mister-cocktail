f3abc6342dd9675d167dd1069a0236d9810fed44
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2019-11-15 12:26:08 +0100

    done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,6 +1,7 @@
 class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show]
 
-  def home
+  def index
     @cocktails = Cocktail.all
   end
 
@@ -8,8 +9,25 @@ class CocktailsController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(set_cocktail)
+    if @cocktail.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def set_cocktail
+    params.require(:cocktail).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'cocktails#home'
+  root to: 'cocktails#index'
   resources :cocktails, only: [:show, :new, :create] do
-    resources :doses, only: [:new, :create, :destroy]
+    resources :doses, only: [:new, :create]
   end
+  resources :doses, only: [:destroy]
 end
